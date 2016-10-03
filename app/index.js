d1c2a9a952c23d738f9c6e6a3cb0b821bf3f5503
linesh <linesh.simpcity@gmail.com>
2016-10-03 23:47:40 +0800

    Linesh: Use ES6 arrow function to write react component

--- app/index.js
+++ app/index.js
@@ -1,14 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const HelloWorld = React.createClass({
-   render: function () {
-      return (
-         <div>
-            Hello World!
-         </div>
-      )
-   }
-});
+const HelloWorld = () => (
+   <div>Hello World</div>
+)
 
 ReactDOM.render(<HelloWorld />, document.getElementById('app'))

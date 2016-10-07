42cb81ec22f87054cff2dd29fe60ecd8f7d47ee7
linesh <linesh.simpcity@gmail.com>
2016-10-07 16:47:02 +0800

    Linesh: Passing data through parent/child components

--- app/index.js
+++ app/index.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
-const HelloWorld = ({ name }) => (
-   <div>Hello {name ? name : 'World'}</div>
-)
+class Hello extends Component {
+   constructor(props) {
+      super(props)
+      this.state = {
+         name: 'Linesh',
+         friends: ['Jimmy', 'Shiny', 'Ear']
+      }
+   }
 
-ReactDOM.render(<HelloWorld name="Linesh"/>, document.getElementById('app'))
+   render() {
+      const { name, friends } = this.state
+      return (
+         <div className="hello">
+            <h3>Welcome, { name }</h3>
+            <List friends={ friends }></List>
+         </div>
+      )
+   }
+}
+
+class List extends Component {
+   constructor(props) {
+      super(props)
+   }
+
+
+   render() {
+      const friends = this.props.friends.map(friend => (
+         <li>{friend}</li>
+      ))
+      return (
+         <div className="friends">
+            <h3>Friends</h3>
+            <ul>
+               {friends}
+            </ul>
+         </div>
+      )
+   }
+}
+ReactDOM.render(<Hello />, document.getElementById('app'))

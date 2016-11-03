132f2c8c450bda60f11abc2faa7e772dfac0ee53
linesh <linesh.simpcity@gmail.com>
2016-11-03 22:30:06 +0800

    Linesh: Introduce raven js to track bugs

--- app/index.js
+++ app/index.js
@@ -1,5 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Routes } from './config/router'
+import Raven from 'raven-js'
 
+const sentry = {
+   app: '111723',
+   key: 'fb773fbcce3c4e85871beb26f117d606'
+}
+const sentryUrl = `https://${sentry.key}@sentry.io/${sentry.app}`
+
+Raven.config(sentryUrl).install()
 ReactDOM.render(Routes, document.getElementById('app'))

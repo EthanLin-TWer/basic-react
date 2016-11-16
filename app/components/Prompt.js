4c00f53ad5a140375b50ffc522c2e5c57752ae06
linesh <linesh.simpcity@gmail.com>
2016-11-16 18:28:14 +0800

    Linesh: Refactor component to ES6 class with lifecycle method

--- app/components/Prompt.js
+++ app/components/Prompt.js
@@ -1,35 +1,44 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes, Component } from 'react'
 
-export const Prompt = ({ username, header, onSubmitUser, onUpdateUser }) => {
-   return (
-      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center">
-         <h1>{header}</h1>
-         <div className="col-sm-12">
-            <form onSubmit={onSubmitUser}>
-               <div className="form-group">
-                  <input type="text"
-                         className="form-control"
-                         placeholder="Github username"
-                         onChange={onUpdateUser}
-                         value={username}
-                  />
-               </div>
-               <div className="form-group col-sm-4 col-sm-offset-4">
-                  <button className="btn btn-block btn-success"
-                          type="submit">
-                     Continue
-                  </button>
-               </div>
-            </form>
+export class Prompt extends Component {
+   render() {
+      const { username, header, onSubmitUser, onUpdateUser } = this.props
+      return (
+         <div className="jumbotron col-sm-6 col-sm-offset-3 text-center">
+            <h1>{header}</h1>
+            <div className="col-sm-12">
+               <form onSubmit={onSubmitUser}>
+                  <div className="form-group">
+                     <input type="text"
+                            className="form-control"
+                            placeholder="Github username"
+                            onChange={onUpdateUser}
+                            value={username}
+                     />
+                  </div>
+                  <div className="form-group col-sm-4 col-sm-offset-4">
+                     <button className="btn btn-block btn-success"
+                             type="submit">
+                        Continue
+                     </button>
+                  </div>
+               </form>
+            </div>
          </div>
-      </div>
-   )
+      )
+   }
+
+   componentWillReceiveProps(nextProps, nextContext) {
+      console.log('component will receive props')
+   }
+
+   static propTypes = {
+      username: PropTypes.string.isRequired,
+      header: PropTypes.string.isRequired,
+      onUpdateUser: PropTypes.func.isRequired,
+      onSubmitUser: PropTypes.func.isRequired
+   }
+
 }
 
-Prompt.propTypes = {
-   username: PropTypes.string.isRequired,
-   header: PropTypes.string.isRequired,
-   onUpdateUser: PropTypes.func.isRequired,
-   onSubmitUser: PropTypes.func.isRequired
-}
 

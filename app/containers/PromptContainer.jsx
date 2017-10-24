c28a86f01d9a9b7ceea049589d811446506dde5e
Linesh <linesh.simpcity@gmail.com>
2017-10-25 00:44:03 +0800

    [Linesh] 🔧Chore: change jsx files to .jsx extensions

---
+++ app/containers/PromptContainer.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { Prompt } from '../components/Prompt'
+
+export class PromptContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { username: '' }
+
+    this.handleUpdateUser = this.handleUpdateUser.bind(this)
+    this.handleSubmitUser = this.handleSubmitUser.bind(this)
+  }
+
+  static contextTypes = {
+    router: React.PropTypes.object.isRequired
+  }
+
+  handleUpdateUser(event) {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handleSubmitUser(event) {
+    event.preventDefault()
+    const { router, routeParams } = this.props
+    const current = this.state.username;
+    this.setState({ username: '' })
+
+    if (!routeParams.playerOne) {
+      router.push(`/playerTwo/${current}`)
+    } else {
+      router.push({
+        pathname: '/battle',
+        query: {
+          playerOne: routeParams.playerOne,
+          playerTwo: this.state.username
+        }
+      })
+    }
+  }
+
+  render() {
+    const { route } = this.props
+    return (
+      <Prompt username={ this.state.username } header={ route.header }
+              onUpdateUser={ this.handleUpdateUser }
+              onSubmitUser={ this.handleSubmitUser }
+      />
+
+    )
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,35 @@ const webpack = require('webpack')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ProvidePluginConfig = new webpack.ProvidePlugin({
-   $: 'jquery',
-   jQuery: 'jquery'
+  $: 'jquery',
+  jQuery: 'jquery'
 });
 const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-   template: __dirname + '/app/index.html',
-   filename: 'index.html',
-   inject: 'body'
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
 })
 
 module.exports = {
-   entry: './app/index.js',
-   module: {
-      loaders: [
-         {
-            test: /\.(js|jsx)$/,
-            exclude: ['node_modules'],
-            loader: 'babel-loader'
-         }
-      ]
-   },
-   output: {
-      path: __dirname + '/dist',
-      filename: 'bundle.js'
-   },
-   plugins: [
-      HTMLWebpackPluginConfig,
-      ProvidePluginConfig
-   ]
+  entry: './app/index.js',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: ['node_modules'],
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    HTMLWebpackPluginConfig,
+    ProvidePluginConfig
+  ]
 }
